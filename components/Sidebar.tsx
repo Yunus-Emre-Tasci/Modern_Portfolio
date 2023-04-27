87d0a5d0c23d6f5d797c98b4719fdb2de37dbcae
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-04-27 22:41:12 +0300

    sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -19,11 +19,15 @@ const Sidebar = () => {
       </h3>
 
       <p className="px-2 py-1 my-3 bg-gray-200 rounded-full">Web Developer</p>
-      <a className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full" href="" download="name">
+      <a
+        className="flex items-center justify-center px-2 py-1 my-3 bg-gray-200 rounded-full"
+        href=""
+        download="name"
+      >
         {" "}
         <GiTie className="w-6 h-6" /> Download Resume
       </a>
-      <div className="flex justify-around w-3/4 mx-auto my-5 text-blue-500 md:w-full">
+      <div className="flex justify-around w-3/4 mx-auto my-5 text-orange-500 md:w-full">
         <a href="">
           <AiFillYoutube className="w-8 h-8" />
         </a>
@@ -34,16 +38,16 @@ const Sidebar = () => {
           <AiFillLinkedin className="w-8 h-8" />
         </a>
       </div>
-      <div>
-        <div>
+      <div className="py-4 my-5 bg-gray-200 mx-[-1rem]">
+        <div className="flex items-center justify-center space-x-2">
           <GoLocation />
           <span>Turkey</span>
         </div>
-        <p>yunusemretasci092@gmail.com</p>
-        <p>0535 104 58 85</p>
+        <p className="my-2">yunusemretasci092@gmail.com</p>
+        <p className="my-2">0535 104 58 85</p>
       </div>
-      <button>Email</button>
-      <button>jmj</button>
+      <button className="w-8/12 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-orange-400 to-blue-400">Email</button>
+      <button className="w-8/12 px-5 py-2 my-2 text-white rounded-full bg-gradient-to-r from-orange-400 to-blue-400">Toggle Theme</button>
     </div>
   );
 }
