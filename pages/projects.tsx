3b256534d24cb139c80a2493cb3178971875c858
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-04-29 21:54:59 +0300

    animate-project

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,6 +3,8 @@ import ProjectCard from "../components/ProjectCard";
 import ProjectsNavbar from "../components/ProjectsNavbar";
 import { projects as projectsData } from "../data";
 import { Category } from "../types";
+import {motion} from "framer-motion"
+import { fadeinup, stagger } from "@/animationsdeneme2";
 
 const Projects = () => {
   const [projects, setProjects] = useState(projectsData);
@@ -24,22 +26,28 @@ const Projects = () => {
 
   return (
     <div className="px-5 py-2 overflow-y-scroll" style={{ height: "65vh" }}>
-        <h2>Navbar</h2>
+      <h2>Navbar</h2>
       <ProjectsNavbar
         handlerFilterCategory={handlerFilterCategory}
         active={active}
       />
 
-      <div className="relative grid grid-cols-12 gap-4 my-3">
+      <motion.div
+        variants={stagger}
+        initial="initial"
+        animate="animate"
+        className="relative grid grid-cols-12 gap-4 my-3"
+      >
         {projects.map((project) => (
-          <div
+          <motion.div
+            variants={fadeinup}
             key={project.name}
             className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200"
           >
             <ProjectCard project={project} />
-          </div>
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 };
