d1f1ed2091432aed031b765ae51dfb73ede72441
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-04-29 16:08:23 +0300

    ProjectCard

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { useState } from "react";
+import ProjectCard from "../components/ProjectCard";
+import ProjectsNavbar from "../components/ProjectsNavbar";
+import { projects as projectsData } from "../data";
+import { Category } from "../types";
 
-const projects = () => {
+const Projects = () => {
+  const [projects, setProjects] = useState(projectsData);
+  const [active, setActive] = useState("all");
+
+  const handlerFilterCategory = (category: Category | "all") => {
+    if (category === "all") {
+      setProjects(projectsData);
+      setActive(category);
+      return;
+    }
+
+    const newArray = projectsData.filter((project) =>
+      project.category.includes(category)
+    );
+    setProjects(newArray);
+    setActive(category);
+  };
+
   return (
-    <div className='p-4'>projects</div>
-  )
-}
+    <div className="px-5 py-2 overflow-y-scroll" style={{ height: "65vh" }}>
+        <h2>Navbar</h2>
+      {/* <ProjectsNavbar
+        handlerFilterCategory={handlerFilterCategory}
+        active={active}
+      /> */}
 
-export default projects
+      <div className="relative grid grid-cols-12 gap-4 my-3">
+        {projects.map((project) => (
+          <div
+            key={project.name}
+            className="col-span-12 p-2 bg-gray-200 rounded-lg sm:col-span-6 lg:col-span-4 dark:bg-dark-200"
+          >
+            <ProjectCard project={project} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
